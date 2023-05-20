bc1721d8ea66df847ec0c5c8d1f1727a16cccf9b
Steve <sjd734312@gmail.com>
2023-05-20 09:17:01 -0400

    arth. finished

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.ceil(Math.random() * 10) + 1;
+
+const num3 = 4;
+const num4 = 11;
+const mod = num3 % num4;
+
+let max;
+max = Math.max(1, 2, 3, 5, 20);
